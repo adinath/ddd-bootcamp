ff3e7417ab27940ce331752f7ff5aeb6f8cd9f81
adi <adinaths@gmail.com>
2017-09-02 16:24:32 +0530

    add gradle wrapper in sample

--- create_service.sh
+++ create_service.sh
@@ -15,6 +15,8 @@ mkdir -p src/test/java
 cp ../sample-domain/build.gradle .
 cp ../sample-domain/gradlew .
 
+cp -r ../sample-domain/gradle ./gradle
+
 echo '# Dockerfile - totp service
 FROM java:8-jre
 
